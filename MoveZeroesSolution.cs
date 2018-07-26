7ff07ec4a4b0eea0d920d35bb1fbed5ae7576896
yli152 <41751701+yli152@users.noreply.github.com>
2018-07-26 15:14:55 -0400

    Add files via upload

---
+++ MoveZeroesSolution.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    public class MoveZeroesSolution
+    {
+        //Traverse Twice. First: assign all non 0 values to the left. Second: fill the remains with 0
+        public void MoveZeroes(int[] nums)
+        {
+            int len = nums.Length;
+
+            if (nums.Length == 0)
+            {
+                return;
+            }
+
+            int index = 0;
+            for (int i = 0; i < len; i++) {
+                if (nums[i] != 0) {
+                    nums[index++] = nums[i];
+                }
+            }
+
+            int fillindex = index;
+            while (fillindex < len) {
+                nums[fillindex++] = 0;
+            }
+        }
+    }
+}
