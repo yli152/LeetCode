db15720dfa35b4ca8821ed403301b090c4ae1ed7
yli152 <41751701+yli152@users.noreply.github.com>
2018-07-26 13:44:16 -0400

    Add files via upload

---
+++ RotateArraySolution.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    public class RotateArraySolution
+    {
+        public void Rotate(int[] nums, int k) {
+            //Time Limited:
+            //-----------------------------------------
+            //int len = nums.Length;
+            //int number = 0;
+            //for (int i = 0; i < k; i++) {
+            //    number = nums[len - 1];
+            //    for (int j = len - 1; j > 0; j--) {
+            //        nums[j] = nums[j - 1];
+            //    }
+            //    nums[0] = number;
+            //}
+        }
+    }
+}
