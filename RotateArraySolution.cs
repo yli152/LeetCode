a811ca08e9f483fa1c5e253af3096ec1f95cecc9
yli152 <41751701+yli152@users.noreply.github.com>
2018-07-26 14:00:25 -0400

    Add files via upload

---
+++ RotateArraySolution.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    public class RotateArraySolution
+    {
+        public void Rotate(int[] nums, int k) {
+            //Time Limited:
+            //-----------------------------------------
+            //int len = nums.Length;
+            //int number = 0;
+            //for (int i = 0; i < k; i++) {
+            //    number = nums[len - 1];
+            //    for (int j = len - 1; j > 0; j--) {
+            //        nums[j] = nums[j - 1];
+            //    }
+            //    nums[0] = number;
+            //}
+            //Solution:
+            //-------------------------------------------
+            int len = nums.Length;
+            k %= len;
+            Reverse(nums, 0, len - 1);
+            Reverse(nums, 0, k - 1);
+            Reverse(nums, k, len - 1);
+        }
+
+        public void Reverse(int[] nums, int start, int end) {
+            while (start < end) {
+                int temp = nums[start];
+                nums[start] = nums[end];
+                nums[end] = temp;
+                start++;
+                end--;
+            }
+        }
+    }
+}
