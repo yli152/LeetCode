a2ce8d32e88e7af037cd60e199ba5b22ac074057
yli152 <41751701+yli152@users.noreply.github.com>
2018-07-26 16:19:32 -0400

    Add files via upload

---
+++ RemoveElementSolution.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    public class RemoveElementSolution
+    {
+        //Like MoveZeros !No need to delete real element, just find the first x elements not val (return that amount --> length)
+        public int RemoveElement(int[] nums, int val) {
+            int len = nums.Length;
+            if (len == 0) {
+                return 0;
+            }
+
+            int index = 0;
+            for (int i = 0; i < len; i++) {
+                if (nums[i] != val) {
+                    nums[index++] = nums[i];
+                }
+            }
+            return index;
+        } 
+    }
+}
