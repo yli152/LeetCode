a287fdde499239a8405968cd9c1a9b8d12433350
yli152 <41751701+yli152@users.noreply.github.com>
2018-07-27 10:30:59 -0400

    Add files via upload

---
+++ RemoveDuplicatesFromSortedArraySolution.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    public class RemoveDuplicatesFromSortedArraySolution
+    {
+        //Same with "Remove Element" using nums[index]
+        public int RemoveDuplicates(int[] nums) {
+            int len = nums.Length;
+            if (len == 0) {
+                return 0;
+            }
+            if (len == 1) {
+                return 1;
+            }
+
+            int index = 1;
+            for (int i = 1; i < len; i++) {
+                if (nums[i] != nums[i - 1]) {
+                    nums[index++] = nums[i];
+                }
+            }
+
+            return index;
+        }
+    }
+}
