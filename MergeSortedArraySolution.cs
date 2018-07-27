4064e7cc2e0d0731c388f68dd584380eaf9530b8
yli152 <41751701+yli152@users.noreply.github.com>
2018-07-27 15:59:11 -0400

    Add files via upload

---
+++ MergeSortedArraySolution.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    public class MergeSortedArraySolution
+    {
+        //back from nums1 (m + n - 1) to fill each position
+        public void Merge(int[] nums1, int m, int[] nums2, int n) {
+            int i = m - 1;
+            int j = n - 1;
+            int k = m + n - 1;
+
+            while (i >= 0 && j >= 0) {
+                if (nums1[i] > nums2[j]) {
+                    nums1[k--] = nums1[i--];
+                } else {
+                    nums1[k--] = nums2[j--];
+                }
+            }
+
+            while (j >= 0) {
+                nums1[k--] = nums2[j--];
+            }
+        }
+    }
+}
