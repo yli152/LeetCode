854477cc4e94f4e3dd7fcd4fe4ebf98c02ab192e
yli152 <41751701+yli152@users.noreply.github.com>
2018-07-27 09:20:05 -0400

    Add files via upload

---
+++ BestTimeToBuyAndSellStockSolution.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    public class BestTimeToBuyAndSellStockSolution
+    {
+        //DP to store current max profits, everytime current prices element compares with previous dp[] element using min()
+        public int MaxProfit(int[] prices) {
+            int len = prices.Length;
+            int[] dp = new int[len];
+            if (prices.Length == 0 || prices.Length == 1)
+            {
+                return 0;
+            }
+            else
+            {
+                int min = Math.Min(prices[0], prices[1]);
+                dp[0] = 0;
+                dp[1] = prices[1] - prices[0];
+
+                for (int i = 2; i < len; i++)
+                {
+                    dp[i] = prices[i] - min;
+                    if (prices[i] < min)
+                    {
+                        min = prices[i];
+                    }
+                }
+
+                int result = -1;
+                if (dp[1] <= 0)
+                {
+                    result = 0;
+                }
+                else
+                {
+                    result = dp[1];
+                }
+
+                for (int j = 0; j < len; j++)
+                {
+                    if (dp[j] > result)
+                    {
+                        result = dp[j];
+                    }
+                }
+
+                return result;
+            }
+        }
+    }
+}
