b7360e4893168f05facf5d2fadd76990ebc222c6
yli152 <41751701+yli152@users.noreply.github.com>
2018-07-30 10:13:06 -0400

    Add files via upload

---
+++ MissingNumberSolution.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    public class MissingNumberSolution
+    {
+        //sum - actualsum
+        public int MissingNumber(int[] nums) {
+            int len = nums.Length;
+            int sum = (1 + len) * len / 2;
+
+            int actualSum = 0;
+            for (int i = 0; i <len; i++) {
+                actualSum += nums[i];
+            }
+
+            return sum - actualSum;
+        }
+    }
+}
