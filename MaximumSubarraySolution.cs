52b7f98b3ce4f5307a827ed821890ffd71dff61f
yli152 <41751701+yli152@users.noreply.github.com>
2018-07-30 16:34:50 -0400

    Add files via upload

---
+++ MaximumSubarraySolution.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    public class MaximumSubarraySolution
+    {
+        public int MaxSubArray(int[] nums)
+        {
+            //DP --> everytime check if dp[i - 1] > 0
+            int len = nums.Length;
+            int[] dp = new int[len];
+            dp[0] = nums[0];
+            int max = dp[0];
+            for (int i = 1; i < len; i++)
+            {
+                if (dp[i - 1] > 0)
+                {
+                    dp[i] = dp[i - 1] + nums[i];
+                }
+                else
+                {
+                    dp[i] = nums[i];
+                }
+
+                if (dp[i] > max)
+                {
+                    max = dp[i];
+                }
+            }
+
+            return max;
+        }
+    }
+}
