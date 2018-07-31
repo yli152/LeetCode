aed12892ce12faae13e9f026809f7458d0acbbfb
yli152 <41751701+yli152@users.noreply.github.com>
2018-07-31 16:38:58 -0400

    Add files via upload

---
+++ MajorityElementSolution.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    public class MajorityElementSolution
+    {
+        //Dictionary
+        public int MajorityElement(int[] nums) {
+            Dictionary<int, int> dc = new Dictionary<int, int>();
+            int len = nums.Length;
+            int max = 0;
+            int me = -1;
+
+            for (int i = 0; i < len; i++) {
+                if (dc.ContainsKey(nums[i]))
+                {
+                    dc[nums[i]]++;
+                }
+                else {
+                    dc[nums[i]] = 1;
+                }
+            }
+
+            foreach (var item in dc) {
+                if (item.Value > max) {
+                    max = item.Value;
+                    me = item.Key;
+                }
+            }
+            return me;
+        }
+    }
+}
