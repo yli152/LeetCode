9f64a7eedd10eba988ed7a995883ddcd396ab0ee
yli152 <41751701+yli152@users.noreply.github.com>
2018-08-01 13:19:36 -0400

    Add files via upload

---
+++ ContainsDuplicateIISolution.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    public class ContainsDuplicateIISolution
+    {
+        //Dictionary
+        public bool ContainsNearbyDuplicate(int[] nums, int k) {
+            Dictionary<int, int> dc = new Dictionary<int, int>();
+            int len = nums.Length;
+            bool flag = false;
+
+            for (int i = 0; i < len; i++) {
+                if (dc.Keys.Contains(nums[i])) {
+                    if (i - dc[nums[i]] <= k) {
+                        flag = true;
+                    }
+                    dc[nums[i]] = i;
+                } else {
+                    dc[nums[i]] = i;
+                }
+            }
+
+            return flag;
+        }
+    }
+}
