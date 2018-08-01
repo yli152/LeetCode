56cea5c46397f2a662cf7d0cea7971343d986f98
yli152 <41751701+yli152@users.noreply.github.com>
2018-08-01 13:57:33 -0400

    Add files via upload

---
+++ TwoSumSolution.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    public class TwoSumSolution
+    {
+        //Dictionary
+        public int[] TwoSum(int[] nums, int target) {
+            Dictionary<int, int> dc = new Dictionary<int, int>();
+            int len = nums.Length;
+            int[] result = new int[2];
+
+            for (int i = 0; i < len; i++)
+            {
+                if (dc.Keys.Contains(target - nums[i]))
+                {
+                    result[0] = i;
+                    result[1] = dc[target - nums[i]];
+                    return result;
+                }
+                else
+                {
+                    dc[nums[i]] = i;
+                }
+            }
+
+            return result;
+        }
+    }
+}
