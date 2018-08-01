5b17102ad3291ff23f94e622cdbcc7774f328360
yli152 <41751701+yli152@users.noreply.github.com>
2018-08-01 11:24:15 -0400

    Add files via upload

---
+++ ContainsDuplicateSolution.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    public class ContainsDuplicateSolution
+    {
+        //Dictionary
+        public bool ContainsDuplicate(int[] nums) {
+            Dictionary<int, int> dc = new Dictionary<int, int>();
+            int len = nums.Length;
+
+            for (int i = 0; i < len; i++) {
+                if (dc.Keys.Contains(nums[i]))
+                {
+                    return true;
+                }
+                else {
+                    dc[nums[i]] = 1;
+                }
+            }
+
+            return false;
+        }
+    }
+}
