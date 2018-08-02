c6b43c8e9a7afdffe32e937bfc2c53d8ed93f14a
yli152 <41751701+yli152@users.noreply.github.com>
2018-08-02 11:41:18 -0400

    Add files via upload

---
+++ FindAllNumbersDisappearedInAnArraySolution.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    // Swap solution !nums[i] - 1 --> index      nums[i] <--> nums[index]
+    public class FindAllNumbersDisappearedInAnArraySolution
+    {
+        public List<int> FindDisappearedNumbers(int[] nums) {
+            List<int> result = new List<int>();
+            int len = nums.Length;
+
+            if (len == 0) {
+                return result;
+            }
+
+            int i = 0;
+            int index;
+
+            while (i < len) {
+                if (nums[i] == i + 1) {
+                    i++;
+                    continue;
+                }
+
+                index = nums[i] - 1;
+
+                if (nums[i] == nums[index]) {
+                    i++;
+                    continue;
+                }
+
+                int temp = nums[i];
+                nums[i] = nums[index];
+                nums[index] = temp;
+            }
+
+            for (int j = 0; j < len; j++) {
+                if (nums[j] != j + 1) {
+                    result.Add(j + 1);
+                }
+            }
+            return result;
+        }
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    //public List<Integer> findDisappearedNumbers(int[] nums)
+    //{
+
+    //    List<Integer> missing = new ArrayList<>();
+    //    int[] a = nums;
+
+    //    if (a.length == 0)
+    //    {
+    //        return missing;
+    //    }
+
+    //    int j;
+    //    int i = 0;
+
+    //    while (i < a.length)
+    //    {
+    //        if (a[i] == i + 1)
+    //        {
+    //            i++;
+    //            continue;
+    //        }
+
+    //        j = a[i] - 1;
+
+    //        if (a[i] == a[j])
+    //        {
+    //            i++;
+    //            continue;
+    //        }
+
+    //        int t = a[i];
+    //        a[i] = a[j];
+    //        a[j] = t;
+    //    }
+
+    //    for (i = 0; i < a.length; i++)
+    //    {
+    //        if (a[i] != i + 1)
+    //        {
+    //            missing.add(i + 1);
+    //        }
+    //    }
+
+    //    return missing;
+    //}
+}
