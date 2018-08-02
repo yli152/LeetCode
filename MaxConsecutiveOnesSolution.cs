db3e4bd8383c153133eade61f98990701584fa66
yli152 <41751701+yli152@users.noreply.github.com>
2018-08-02 15:29:35 -0400

    Add files via upload

---
+++ MaxConsecutiveOnesSolution.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    public class MaxConsecutiveOnesSolution
+    {
+        //count, max
+        public int FindMaxConsecutiveOnes(int[] nums) {
+            int len = nums.Length;
+            int count = 0;
+            int max = 0;
+
+            if (len == 0)
+            {
+                return 0;
+            }
+
+            if (nums[0] == 1)
+            {
+                count++;
+                max++;
+            }
+
+            for (int i = 1; i < len; i++)
+            {
+                if (nums[i] == 1)
+                {
+                    count++;
+                }
+                else
+                {
+                    count = 0;
+                }
+                if (count > max)
+                {
+                    max = count;
+                }
+            }
+
+            return max;
+        }
+    }
+}
