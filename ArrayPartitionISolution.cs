d2a661a0308d773ee2d41e342b9b114ca9b9cf0a
yli152 <41751701+yli152@users.noreply.github.com>
2018-08-03 10:30:00 -0400

    Add files via upload

---
+++ ArrayPartitionISolution.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    public class ArrayPartitionISolution
+    {
+        //Sorting first and then add all elements in odd index
+        public int ArrayPairSum(int[] nums) {
+            Array.Sort(nums);
+            int len = nums.Length;
+            int result = 0;
+
+            for (int i = 0; i < len; i += 2) {
+                result += nums[i];
+            }
+
+            return result;
+        }
+    }
+}
