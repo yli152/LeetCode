4c3ea377758c88b7472125f0e535bc3b2156c512
yli152 <41751701+yli152@users.noreply.github.com>
2018-08-06 16:24:07 -0400

    Add files via upload

---
+++ MaximumAverageSubarrayISolution.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    public class MaximumAverageSubarrayISolution
+    {
+        //slide window
+        public double FindMaxAverage(int[] nums, int k) {
+            int len = nums.Length;
+
+            if (len < k) {
+                return 0;
+            }
+            int max = 0;
+
+            for (int i = 0; i < k; i++) {
+                max += nums[i];
+            }
+
+            int sum = max;
+
+            for (int i = 1; i < len - k + 1; i++) {
+                sum = sum - nums[i - 1] + nums[i + k - 1];
+
+                if (sum > max) {
+                    max = sum;
+                }
+            }
+
+            return max;
+        }
+    }
+}
