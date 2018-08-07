29907e59cea7665fc3c93810b2ec1a9830a7a7a0
yli152 <41751701+yli152@users.noreply.github.com>
2018-08-07 16:45:48 -0400

    Add files via upload

---
+++ LongestContinuousIncreasingSubsequenceSolution.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    public class LongestContinuousIncreasingSubsequenceSolution
+    {
+        public int FindLengthOfLCIS(int[] nums) {
+            int len = nums.Length;
+            if (len == 0)
+            {
+                return 0;
+            }
+
+            int result = 1;
+            int count = 1;
+
+            for (int i = 0; i < len - 1; i++)
+            {
+                if (nums[i + 1] > nums[i])
+                {
+                    count++;
+                }
+                else
+                {
+                    if (count > result)
+                    {
+                        result = count;
+                    }
+                    count = 1;
+                }
+            }
+
+            if (count > result)
+            {
+                result = count;
+            }
+
+            return result;
+        }
+    }
+}
