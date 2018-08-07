556b7a65597036ecc8751dc431589f0bf92c1694
yli152 <41751701+yli152@users.noreply.github.com>
2018-08-07 10:07:21 -0400

    Add files via upload

---
+++ ImageSmootherSolution.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    public class ImageSmootherSolution
+    {
+        //O(N)2 check all the index surronding M[i, j]
+        public int[,] ImageSmoother(int[,] M) {
+            int row = M.GetLength(0);
+            int col = M.GetLength(1);
+            int[,] result = new int[row, col];
+            double count = 0;
+            double sum = 0;
+
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < col; j++)
+                {
+                    sum = M[i, j];
+                    count = 1;
+
+                    if (i - 1 >= 0)
+                    {
+                        sum += M[i - 1, j];
+                        count++;
+                    }
+                    if (i + 1 < row)
+                    {
+                        sum += M[i + 1, j];
+                        count++;
+                    }
+                    if (j - 1 >= 0)
+                    {
+                        sum += M[i, j - 1];
+                        count++;
+                    }
+                    if (j + 1 < col)
+                    {
+                        sum += M[i, j + 1];
+                        count++;
+                    }
+                    if (i - 1 >= 0 && j - 1 >= 0)
+                    {
+                        sum += M[i - 1, j - 1];
+                        count++;
+                    }
+                    if (i + 1 < row && j - 1 >= 0)
+                    {
+                        sum += M[i + 1, j - 1];
+                        count++;
+                    }
+                    if (i - 1 >= 0 && j + 1 < col)
+                    {
+                        sum += M[i - 1, j + 1];
+                        count++;
+                    }
+                    if (i + 1 < row && j + 1 < col)
+                    {
+                        sum += M[i + 1, j + 1];
+                        count++;
+                    }
+                    result[i, j] = (int)Math.Floor(sum / count);
+                }
+            }
+
+            return result;
+        }
+    }
+}
