232cc4591b41de682b14e344985a0c75d837de5b
yli152 <41751701+yli152@users.noreply.github.com>
2018-08-08 16:09:12 -0400

    Add files via upload

---
+++ FindPivotIndexSolution.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    //2O(N) sumr等于总和，然后遍历sumr减一个和suml比较，没有的话suml再加上这个
+    public class FindPivotIndexSolution
+    {
+        public int PivotIndex(int[] nums) {
+            //int left = 0;
+            //int right = nums.Length - 1;
+            //int suml = nums[left];
+            //int sumr = nums[right];
+            //int pivot = -1;
+
+            //while (left < right) {
+            //    if (suml < sumr) {
+            //        left++;
+            //        suml += nums[left];
+            //    }
+            //    if (sumr < suml) {
+            //        right--;
+            //        sumr += nums[right];
+            //    }
+            //    if (suml == sumr) {
+            //        if (right - left == 2) {
+            //            pivot = left + 1;
+            //            return pivot;
+            //        }
+            //    }
+            //}
+
+            //return pivot;
+
+            int len = nums.Length;
+            int sumr = 0;
+            int suml = 0;
+            if (len == 0) {
+                return -1;
+            }
+
+            for (int i = 0; i < len; i++) {
+                sumr += nums[i];
+            }
+
+            for (int i = 0; i < len; i++) {
+                sumr -= nums[i];
+                
+                if (sumr == suml) {
+                    return i;
+                }
+
+                suml += nums[i];
+            }
+
+            return -1;
+        }
+    }
+}
