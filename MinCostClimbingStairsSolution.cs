6554a08ea39147bf1b75901aee70b4805dbd817c
yli152 <41751701+yli152@users.noreply.github.com>
2018-08-09 09:36:09 -0400

    Add files via upload

---
+++ MinCostClimbingStairsSolution.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    public class MinCostClimbingStairsSolution
+    {
+        //Each step compares the last step and the last second step (Math.Min(cost[i - 1], cost[i - 2]))
+        public int MinCostClimbingStairs(int[] cost) {
+            int len = cost.Length;
+            for (int i = 2; i < len; i++)
+            {
+                cost[i] += Math.Min(cost[i - 1], cost[i - 2]);
+            }
+            return Math.Min(cost[len - 1], cost[len - 2]);
+        }
+    }
+}
