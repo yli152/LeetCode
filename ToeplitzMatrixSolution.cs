904ccd53c8202fa47866e8bae08debd79cbde7f0
yli152 <41751701+yli152@users.noreply.github.com>
2018-08-09 10:29:56 -0400

    Add files via upload

---
+++ ToeplitzMatrixSolution.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    public class ToeplitzMatrixSolution
+    {
+        //matrix[i, j] != matrix[i + 1, j + 1]
+        public bool IsToeplitzMatrix(int[,] matrix) {
+            int row = matrix.GetLength(0);
+            int col = matrix.GetLength(1);
+            bool result = true;
+
+            for (int i = 0; i < row; i++) {
+                for (int j = 0; j < col; j++) {
+                    if (i + 1 < row && j + 1 < col) {
+                        if (matrix[i, j] != matrix[i + 1, j + 1]) {
+                            return false;
+                        }
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
