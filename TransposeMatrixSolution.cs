2eacfc7663f8bdd70c1395f56df9867d3d23dfa7
yli152 <41751701+yli152@users.noreply.github.com>
2018-08-09 15:45:10 -0400

    Add files via upload

---
+++ TransposeMatrixSolution.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    public class TransposeMatrixSolution
+    {
+        //result[i][j] = A[j][i]; / two ways to create two dimensional array： 1- int[,] arr = new int[2,3]; 2- int[][] arr = new int[2][]; arr[0] = new int[3]; arr[1] = new int[3];
+        public int[][] Transpose(int[][] A) {
+            int row = A.Length;
+            int col = A[0].Length;
+            int[][] result = new int[col][];
+
+            for (int i = 0; i < col; i++)
+            {
+                result[i] = new int[row];
+                for (int j = 0; j < row; j++)
+                {
+                    result[i][j] = A[j][i];
+                }
+            }
+
+            return result;
+        }
+    }
+}
