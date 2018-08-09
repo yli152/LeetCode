b507527faffe8cf1d7286f1dedfe4f5a93c60c70
yli152 <41751701+yli152@users.noreply.github.com>
2018-08-09 09:57:12 -0400

    Add files via upload

---
+++ LargestNumberAtLeastTwiceOfOthersSolution.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    public class LargestNumberAtLeastTwiceOfOthersSolution
+    {
+        //2O(N)
+        public int DominationIndex(int[] nums) {
+            int len = nums.Length;
+            int max = Int32.MinValue;
+            int result = 0;
+
+            for (int i = 0; i < len; i++) {
+                if (nums[i] > max) {
+                    max = nums[i];
+                    result = i;
+                }
+            }
+
+            for (int i = 0; i < len; i++) {
+                if (nums[i] != max)
+                {
+                    if (2 * nums[i] > max)
+                    {
+                        result = -1;
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
