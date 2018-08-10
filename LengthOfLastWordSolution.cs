75e489a7c27a77ac4d8927cc5c62b60b424a64e5
yli152 <41751701+yli152@users.noreply.github.com>
2018-08-10 15:29:05 -0400

    Add files via upload

---
+++ LengthOfLastWordSolution.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    //split
+    public class LengthOfLastWordSolution
+    {
+        public int LengthOfLastWord(string s) {
+            if (s.Length == 0)
+            {
+                return 0;
+            }
+            string[] arr = s.Split(' ');
+            int len = arr.Length;
+
+            for (int i = len - 1; i >= 0; i--)
+            {
+                if (arr[i] != "")
+                {
+                    return arr[i].Length;
+
+                }
+            }
+
+            return 0;
+        }
+    }
+}
