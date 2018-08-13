b2a869503d7be9c5a3cc7f34c7846af017392eeb
yli152 <41751701+yli152@users.noreply.github.com>
2018-08-13 15:29:12 -0400

    Add files via upload

---
+++ ValidParenthesesSolution.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    public class ValidParenthesesSolution
+    {
+        //stack "{[(" push to stack / "}])" pop() to compare
+        public bool IsValid(string s) {
+            int len = s.Length;
+            Stack<char> st = new Stack<char>();
+            for (int i = 0; i < len; i++)
+            {
+                if (s[i] == '{' || s[i] == '(' || s[i] == '[')
+                {
+                    st.Push(s[i]);
+                }
+                else
+                {
+                    if (st.Count != 0)
+                    {
+                        if (s[i] == '}' && st.Pop() != '{')
+                        {
+                            return false;
+                        }
+                        if (s[i] == ')' && st.Pop() != '(')
+                        {
+                            return false;
+                        }
+                        if (s[i] == ']' && st.Pop() != '[')
+                        {
+                            return false;
+                        }
+                    }
+                    else {
+                        return false;
+                    }
+                }
+            }
+            
+            return true;
+        }
+    }
+}
