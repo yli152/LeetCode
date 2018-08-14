b2c213e7818facbb1d3da1a50c2b66ff96c40b8e
yli152 <41751701+yli152@users.noreply.github.com>
2018-08-14 16:55:19 -0400

    Add files via upload

---
+++ ImplementStrStrSolution.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    public class ImplementStrStrSolution
+    {
+        //two pointer !length of haystack should be greater than needle
+        public int StrStr(string haystack, string needle) {
+            int lenh = haystack.Length;
+            int lenn = needle.Length;
+            if (haystack == needle || lenn == 0)
+            {
+                return 0;
+            }
+            if (lenn > lenh)
+            {
+                return -1;
+            }
+
+            int i = 0, j = 0, result = 0;
+            while (i < lenh && j < lenn)
+            {
+                if (haystack[i] == needle[j])
+                {
+                    i++;
+                    j++;
+                }
+                else
+                {
+                    i = ++result;
+                    j = 0;
+                }
+            }
+
+            if (j == lenn)
+            {
+                return result;
+            }
+            return -1;
+        }
+    }
+}
