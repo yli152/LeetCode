259a40ed1f0d2fcd8a31697041d5021114b75edf
yli152 <41751701+yli152@users.noreply.github.com>
2018-08-15 16:10:04 -0400

    Add files via upload

---
+++ RepeatedSubstringPatternSolution.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    public class RepeatedSubstringPatternSolution
+    {
+        //first s+s, and destroy the first and the last to see if the remaining contains s
+        public bool RepeatedSubstringPattern(string s) {
+            String s2 = s + s;
+            return s2.Substring(1, s2.Length - 1).Contains(s);
+        }
+    }
+}
