9314a8d8f3b9241a8969d897eef598f0dcbf874e
yli152 <41751701+yli152@users.noreply.github.com>
2018-08-15 09:41:00 -0400

    Add files via upload

---
+++ ReverseStringSolution.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    //1.StringBuilder 2.S.toCharArray() --> two pointer 3.creat a new Array
+    public class ReverseStringSolution
+    {
+        public string ReverseString(string s) {
+            StringBuilder result = new StringBuilder();
+            int len = s.Length;
+ 
+            for (int i = len - 1; i >= 0; i--)
+            {
+                result.Append(s[i]);
+            }
+            return result.ToString();
+        }
+    }
+}
