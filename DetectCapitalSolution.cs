3c0b8dbf8020dad85c9a2fa6861e6138075480f6
yli152 <41751701+yli152@users.noreply.github.com>
2018-08-16 10:32:21 -0400

    Add files via upload

---
+++ DetectCapitalSolution.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    public class DetectCapitalSolution
+    {
+        //分情况讨论， 先第一位，然后第二位
+        public bool DetectCapitalUse(string word) {
+            int len = word.Length;
+            if (len == 1)
+            {
+                return true;
+            }
+
+            bool result = true;
+
+            for (int i = 1; i < len; i++)
+            {
+                if (word[0] >= 97 && word[0] <= 122)
+                {
+                    if (word[i] >= 65 && word[i] <= 90)
+                    {
+                        result = false;
+                    }
+                }
+                else
+                {
+                    if (word[1] >= 65 && word[1] <= 90)
+                    {
+                        if (word[i] >= 97 && word[i] <= 122)
+                        {
+                            return false;
+                        }
+                    }
+                    else
+                    {
+                        if (word[i] >= 65 && word[i] <= 90)
+                        {
+                            return false;
+                        }
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
