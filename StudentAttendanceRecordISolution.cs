57406e07262d81128e085eec52face62f8a16703
yli152 <41751701+yli152@users.noreply.github.com>
2018-08-16 16:47:59 -0400

    Add files via upload

---
+++ StudentAttendanceRecordISolution.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    public class StudentAttendanceRecordISolution
+    {
+        // continuous L --> i < len - 1;
+        public bool CheckRecord(string s) {
+            int len = s.Length;
+            int countA = 0;
+            int countL = 1;
+
+            for (int i = 0; i < len - 1; i++) {
+                if (s[i] == 'A') {
+                    countA++;
+                    if (s[i + 1] == 'A') {
+                        countA++;
+                    }
+                }
+                if (s[i] == 'L' && s[i + 1] == 'L')
+                {
+                    countL++;
+                }
+                else {
+                    countL = 1;
+                }
+
+                if (countA > 1 || countL > 2) {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
