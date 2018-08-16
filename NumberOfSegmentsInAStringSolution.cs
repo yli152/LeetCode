65decd6fae35a372787567a28648c4feb154537d
yli152 <41751701+yli152@users.noreply.github.com>
2018-08-16 09:18:39 -0400

    Add files via upload

---
+++ NumberOfSegmentsInAStringSolution.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    public class NumberOfSegmentsInAStringSolution
+    {
+        //split and check if arr[i] == ""
+        public int CountSegments(string s) {
+            string[] arr = s.Split(' ');
+            int len = arr.Length;
+            int result = 0;
+            
+            for (int i = 0; i < len; i++) {
+                if (arr[i] != "") {
+                    result++;
+                }
+            }
+
+            return result;
+        }
+    }
+}
