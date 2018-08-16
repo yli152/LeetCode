81e8a9481cdf93d56a37bc017164c44921a4707c
yli152 <41751701+yli152@users.noreply.github.com>
2018-08-16 11:41:11 -0400

    Add files via upload

---
+++ LongestUncommonSubsequenceISolution.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    public class LongestUncommonSubsequenceISolution
+    {
+        //stupid question
+        public int FindLUSlength(string a, string b) {
+            int lena = a.Length;
+            int lenb = b.Length; 
+
+            if (lena == 0 && lenb == 0) {
+                return -1;
+            }
+
+            if (lena != lenb)
+            {
+                return Math.Max(lena, lenb);
+            }
+            else {
+                if (a == b)
+                {
+                    return -1;
+                }
+                else {
+                    return lena;
+                }
+            }
+        }
+    }
+}
