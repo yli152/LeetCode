0f37266fd8be59ee5438e8dd08102b5d0364c063
yli152 <41751701+yli152@users.noreply.github.com>
2018-08-20 14:30:37 -0400

    Add files via upload

---
+++ ValidPalindromeIISolution.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    public class ValidPalindromeIISolution
+    {
+        //recursive with individually IsPalindrome function
+        public bool ValidPalindrome(string s) {
+            int len = s.Length;
+            int left = -1;
+            int right = len;
+
+            while (left < right) {
+                if (s[left] != s[right]) {
+                    return IsPalindrome(s, left, right - 1) || IsPalindrome(s, left + 1, right);
+                }
+                left++;
+                right--;
+            }
+            return true;
+        }
+
+        public bool IsPalindrome(string s, int l, int r) {
+            while (l < r) {
+                if (s[l] != s[r]) {
+                    return false;
+                }
+                l++;
+                r--;
+            }
+            return true;
+        }
+    }
+}
