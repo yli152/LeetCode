ed0e95f440e60c4654b957eeef696bdcf2be588a
yli152 <41751701+yli152@users.noreply.github.com>
2018-08-20 11:42:05 -0400

    Add files via upload

---
+++ JudgeRouteCircleSolution.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    public class JudgeRouteCircleSolution
+    {
+        //(up == down) && (left == right)
+        public bool JudgeCircle(string moves) {
+            int len = moves.Length;
+            int up = 0;
+            int down = 0;
+            int left = 0;
+            int right = 0;
+
+            for (int i = 0; i < len; i++) {
+                if (moves[i] == 'U') {
+                    up++;
+                }
+
+                if (moves[i] == 'D')
+                {
+                    down++;
+                }
+
+                if (moves[i] == 'L')
+                {
+                    left++;
+                }
+
+                if (moves[i] == 'R')
+                {
+                    right++;
+                }
+            }
+
+            return (up == down) && (left == right);
+        }
+    }
+}
