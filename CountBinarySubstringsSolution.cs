028e573b7308e13cc4ccfdb3253fca1485e6f6c9
yli152 <41751701+yli152@users.noreply.github.com>
2018-08-21 10:31:13 -0400

    Add files via upload

---
+++ CountBinarySubstringsSolution.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    public class CountBinarySubstringsSolution
+    {
+        //prvcount, curcount
+        public int countBinarySubstrings(string s) {
+            int prvCount = 0, curCount = 0, res = 0, len = s.Length;
+            for (int i = 0; i < len; i++)
+            {
+                curCount++;
+                if (i == len - 1 || s[i] != s[i + 1])
+                {
+                    res += Math.Min(prvCount, curCount);
+                    prvCount = curCount;
+                    curCount = 0;
+                }
+            }
+            return res;
+        }
+    }
+}
