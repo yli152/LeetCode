3715059d7d0e787b1ffa5faae3fced16a3839b21
yli152 <41751701+yli152@users.noreply.github.com>
2018-08-21 16:53:04 -0400

    Add files via upload

---
+++ FirstUniqueCharacterInAStringSolution.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    public class FirstUniqueCharacterInAStringSolution
+    {
+        //Dictionary
+        public int FirstUniqChar(string s) {
+            int len = s.Length;
+            Dictionary<char, int> dc = new Dictionary<char, int>();
+
+            for (int i = 0; i < len; i++) {
+                if (dc.Keys.Contains(s[i]))
+                {
+                    dc[s[i]]++;
+                }
+                else {
+                    dc[s[i]] = 1;
+                }
+            }
+
+            for (int i = 0; i < len; i++) {
+                if (dc[s[i]] == 1) {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
