8a16e94c4d91aa4e91be7e734599a8382a5b360b
yli152 <41751701+yli152@users.noreply.github.com>
2018-08-22 15:23:17 -0400

    Add files via upload

---
+++ HouseRobberSolution.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    public class HouseRobberSolution
+    {
+        //DP dp[i] = Math.Max(dp[i - 1], dp[i - 2] + nums[i])
+        public int Rob(int[] nums) {
+            int len = nums.Length;
+            if (len == 0) {
+                return 0;
+            }
+            if (len == 1) {
+                return nums[0];
+            }
+            if (len == 2) {
+                return Math.Max(nums[0], nums[1]);
+            }
+
+            int[] dp = new int[len];
+            dp[0] = nums[0];
+            dp[1] = Math.Max(nums[0], nums[1]);
+            int result = 0;
+
+            for (int i = 2; i < len; i++) {
+                dp[i] = Math.Max(dp[i - 1], dp[i - 2] + nums[i]);
+            }
+
+            return dp[len - 1];
+        }
+    }
+}
