f1332e5e6e3f7d3755692be1e371cb247ac35c29
yli152 <41751701+yli152@users.noreply.github.com>
2018-08-22 15:45:07 -0400

    Add files via upload

---
+++ RangeSumQueryImmutableSolution.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    //DP dp[j] - dp[i - 1]
+    public class RangeSumQueryImmutableSolution
+    {
+        int[] dp;
+        public RangeSumQueryImmutableSolution(int[] nums) {
+            int len = nums.Length;
+            for (int i = 1; i < len; i++) {
+                nums[i] += nums[i - 1];
+            }
+            dp = nums;
+        }
+
+        public int SumRange(int i, int j) {
+            if (i == 0) {
+                return dp[j];
+            }
+            return dp[j] - dp[i - 1];
+        }
+    }
+}
