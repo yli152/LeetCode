6b019d30a9349b89cc58f17117b3c625a6c85fcb
yli152 <41751701+yli152@users.noreply.github.com>
2018-08-22 14:41:33 -0400

    Add files via upload

---
+++ ToLowerCaseSolution.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    public class ToLowerCaseSolution
+    {
+        //ASCII
+        public string ToLowerCase(string str) {
+            int len = str.Length;
+            StringBuilder sb = new StringBuilder();
+            sb.Append(str);
+
+            for (int i = 0; i < len; i++)
+            {
+                if (str[i] >= 65 && str[i] <= 90)
+                {
+                    sb[i] = (char)(str[i] + 32);
+                }
+            }
+
+            return sb.ToString();
+        }
+    }
+}
