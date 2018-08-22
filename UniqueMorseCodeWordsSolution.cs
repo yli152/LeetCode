f29f28ee1b78a93a23a02de08a654bdf797e7542
yli152 <41751701+yli152@users.noreply.github.com>
2018-08-22 11:25:12 -0400

    Add files via upload

---
+++ UniqueMorseCodeWordsSolution.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    public class UniqueMorseCodeWordsSolution
+    {
+        //using list and word[i][j] - 'a' to find the Morse Code
+        public int UniqueMorseRepresentations(string[] words) {
+            string[] re = { ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.." };
+            int len = words.Length;
+            List<string> ls = new List<string>();
+            int result = 0;
+
+            for (int i = 0; i < len; i++) {
+                StringBuilder sb = new StringBuilder();
+                int lenword = words[i].Length;
+                for (int j = 0; j < lenword; j++) {
+                    sb.Append(re[words[i][j] - 'a']);
+                }
+                if (!ls.Contains(sb.ToString())) {
+                    ls.Add(sb.ToString());
+                    result++;
+                }
+            }
+
+            return result;
+        }
+    }
+}
