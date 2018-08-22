b55b73a9ccd82b0a35b5a50e5284cfa16b5beaa1
yli152 <41751701+yli152@users.noreply.github.com>
2018-08-22 16:48:01 -0400

    Add files via upload

---
+++ PalindromeNumberSolution.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    public class PalindromeNumberSolution
+    {
+        //toString()
+        public bool IsPalindrome(int x) {
+            string s = x.ToString();
+            int len = s.Length;
+            if (x < 0) {
+                return false;
+            }
+            if (len == 1) {
+                return true;
+            }
+
+            int left = 0;
+            int right = len - 1;
+            while (left < right) {
+                if (s[left] == s[right])
+                {
+                    left++;
+                    right--;
+                }
+                else {
+                    return false;
+                }
+                
+            }
+
+            return true;
+        }
+    }
+}
