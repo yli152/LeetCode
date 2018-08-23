da36ad9fc87273e6d60e151c9a965eb7e7533db9
yli152 <41751701+yli152@users.noreply.github.com>
2018-08-23 11:36:09 -0400

    Add files via upload

---
+++ PowerOfThreeSolution.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    // % /
+    public class PowerOfThreeSolution
+    {
+        public bool IsPowerOfThree(int n) {
+            while (n % 3 == 0) {
+                n %= 3;
+            }
+
+            return n == 0;
+        }
+
+        //return (n>0 &&  1162261467%n==0);
+    }
+}
