706d203ae643740f3ab26728f8ddb689afe39622
yli152 <41751701+yli152@users.noreply.github.com>
2018-08-23 09:50:29 -0400

    Add files via upload

---
+++ ExcelSheetColumnTitleSolution.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    //A-->0, Z-->25
+    public class ExcelSheetColumnTitleSolution
+    {
+        public string ConvertToTitle(int n) {
+            string result = "";
+            while (n != 0) {
+                char cur = (char)((n - 1) % 26 + 65);
+                n = (n - 1) / 26;
+                result = cur + result;
+            }
+
+            return result;
+        }
+    }
+}
