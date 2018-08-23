7effa10fd7d5734b67ff9d9ab347c2b425922d9b
yli152 <41751701+yli152@users.noreply.github.com>
2018-08-23 11:15:20 -0400

    Add files via upload

---
+++ UglyNumberSolution.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    //分情况除2,3,5
+    public class UglyNumberSolution
+    {
+        public bool IsUgly(int num) {
+            if (num == 0) {
+                return false;
+            }
+
+            while (num % 2 == 0) {
+                num /= 2;
+            }
+
+            while (num % 3 == 0)
+            {
+                num /= 3;
+            }
+
+            while (num % 5 == 0)
+            {
+                num /= 5;
+            }
+
+            return num == 1;
+        }
+    }
+}
