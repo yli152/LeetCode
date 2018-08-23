4ebf44bb10cefa039ea6e54d5a61dec8690b0071
yli152 <41751701+yli152@users.noreply.github.com>
2018-08-23 10:58:44 -0400

    Add files via upload

---
+++ FactorialTrailingZeroesSolution.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    public class FactorialTrailingZeroesSolution
+    {
+        //count how many fives
+        public int TrailingZeroes(int n) {
+            int result = 0;
+
+            while (n != 0) {
+                result += n / 5;
+                n /= 5; 
+            }
+
+            return result;
+        }
+    }
+}
