ab8b160411739e3dfde64039233f72dce617a28e
yli152 <41751701+yli152@users.noreply.github.com>
2018-08-23 14:09:56 -0400

    Add files via upload

---
+++ ReverseIntegerSolution.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    //long.Parse(sb.ToString()) < int.MinValue || long.Parse(sb.ToString()) > int.MaxValue
+    public class ReverseIntegerSolution
+    {
+        public int Reverse(int x) {
+            if (x == 0)
+            {
+                return 0;
+            }
+            string s = x.ToString();
+            int len = s.Length;
+            if (len == 1)
+            {
+                return x;
+            }
+
+            StringBuilder sb = new StringBuilder();
+            int limit = 0;
+            if (x < 0)
+            {
+                limit = 1;
+                sb.Append('-');
+
+            }
+
+            for (int i = len - 1; i >= limit; i--)
+            {
+                if (s[i] != 0)
+                {
+                    sb.Append(s[i]);
+                }
+            }
+
+            if (long.Parse(sb.ToString()) < int.MinValue || long.Parse(sb.ToString()) > int.MaxValue) {
+                return 0;
+            }
+            return (int)long.Parse(sb.ToString());
+        }
+    }
+}
