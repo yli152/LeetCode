d260f2d885a0c722dac0a8584dc1c00e9c8b87cd
yli152 <41751701+yli152@users.noreply.github.com>
2018-08-23 11:41:37 -0400

    Add files via upload

---
+++ PowerOfTwoSolution.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    //% / while 
+    public class PowerOfTwoSolution
+    {
+        public bool IsPowerOfTwo(int n) {
+            if (n == 0) {
+                return false;
+            }
+
+            while (n % 2 == 0) {
+                n /= 2;
+            }
+
+            return n == 1;
+        }
+    }
+}
