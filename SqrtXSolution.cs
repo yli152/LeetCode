fb02e597cb4c353ae1b67a44e9775593179babfb
yli152 <41751701+yli152@users.noreply.github.com>
2018-08-23 14:57:17 -0400

    Add files via upload

---
+++ SqrtXSolution.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    //Look for the critical point: i* i <= x && (i+1)(i+1) > x
+    public class SqrtXSolution
+    {
+        public int MySqrt(int x) {
+            if (x == 0 || x == 1)
+            {
+                return x;
+            }
+
+            for (int i = 1; i <= x / i; i++)
+            {
+                if (i <= x / i && (i + 1) > x / (i + 1))
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
+
+//public int mySqrt(int x)
+//{
+//    if (x == 0) return 0;
+//    for (int i = 1; i <= x / i; i++)
+//        if (i <= x / i && (i + 1) > x / (i + 1))// Look for the critical point: i*i <= x && (i+1)(i+1) > x
+//            return i;
+//    return -1;
+//}
