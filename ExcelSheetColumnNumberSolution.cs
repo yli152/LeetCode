20c4af7d72193c648036c31b185e44b284586f09
yli152 <41751701+yli152@users.noreply.github.com>
2018-08-24 13:46:58 -0400

    Add files via upload

---
+++ ExcelSheetColumnNumberSolution.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    public class ExcelSheetColumnNumberSolution
+    {
+        //sum *= 26; sum += c - 'A' + 1;
+        public int titleToNumber(string s) {
+            if (s == null) {
+                return -1;
+            }
+            int sum = 0;
+
+            foreach (char c in s.ToUpper().ToCharArray()) {
+                sum *= 26;
+                sum += c - 'A' + 1;
+            }
+
+            return sum;
+        }
+    }
+}
