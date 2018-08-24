39c5684d609ae3a455152f1d8776cdc603f59d06
yli152 <41751701+yli152@users.noreply.github.com>
2018-08-24 10:11:54 -0400

    Add files via upload

---
+++ LongestSubstringWithoutRepeatingCharactersSolution.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    public class LongestSubstringWithoutRepeatingCharactersSolution
+    {
+        //sliding window
+        public int LengthOfLongestSubstring(string s) {
+            int len = s.Length;
+            HashSet<char> hs = new HashSet<char>();
+            int max = 0;
+            int i = 0;
+            int j = 0;
+
+            while (i < len && j < len) {
+                if (!hs.Contains(s[j]))
+                {
+                    hs.Add(s[j++]);
+                    max = Math.Max(max, j - i);
+                }
+                else {
+                    hs.Remove(s[i++]);
+                }
+            }
+
+            return max;
+        }
+    }
+}
