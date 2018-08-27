5a283bb5d480dc6783ab94a06ac902a57568f9f7
yli152 <41751701+yli152@users.noreply.github.com>
2018-08-27 14:28:23 -0400

    Add files via upload

---
+++ AddStringsSolution.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    public class AddStringsSolution
+    {
+        //sb.Insert(0, (x + y + carry) % 10); carry = (x + y + carry) / 10;
+        public string AddStrings(string num1, string num2) {
+            StringBuilder sb = new StringBuilder();
+            int carry = 0;
+            int len1 = num1.Length;
+            int len2 = num2.Length;
+
+            for (int i = len1 - 1, j = len2 - 1; i >= 0 || j >= 0 || carry == 1; i--, j--)
+            {
+                int x = i < 0 ? 0 : num1[i] - '0';
+                int y = j < 0 ? 0 : num2[j] - '0';
+                sb.Insert(0, (x + y + carry) % 10);
+                carry = (x + y + carry) / 10;
+            }
+
+            return sb.ToString();
+        }
+    }
+}
