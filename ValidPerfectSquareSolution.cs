df739914ca97c9089a7f02da437c5c8cd6c7c526
yli152 <41751701+yli152@users.noreply.github.com>
2018-08-27 10:03:31 -0400

    Add files via upload

---
+++ ValidPerfectSquareSolution.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    public class ValidPerfectSquareSolution
+    {
+        //Binary Search (mid == num / mid && num % mid == 0)
+        public bool IsPerfectSquare(int num) {
+            int left = 1;
+            int right = num;
+
+            while (left <= right)
+            {
+                int mid = left + (right - left) / 2;
+                if (mid == num / mid && num % mid == 0)
+                {
+                    return true;
+                }
+                else if (mid > num / mid)
+                {
+                    right = mid - 1;
+                }
+                else
+                {
+                    left = mid + 1;
+                }
+            }
+
+            return false;
+        }
+    }
+}
