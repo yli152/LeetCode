9f6633017e6a91c17c2a379d2af9d5a9b4895b66
yli152 <41751701+yli152@users.noreply.github.com>
2018-08-27 15:04:05 -0400

    Add files via upload

---
+++ ArrangingCoinsSolution.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    public class ArrangingCoinsSolution
+    {
+        public int ArrangeCoins(int n) {
+            int num = n;
+            int result = 0;
+
+            for (int i = 1; n - i >= 0; i++) {
+                n -= i;
+                result++;
+            }
+
+            return result;
+        }
+    }
+}
