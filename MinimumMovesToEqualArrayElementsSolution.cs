7bc8007f8c87d757a244e5f1f2fa1eb22b6ffb6a
yli152 <41751701+yli152@users.noreply.github.com>
2018-08-27 16:33:30 -0400

    Add files via upload

---
+++ MinimumMovesToEqualArrayElementsSolution.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    //return sum - min * len
+    public class MinimumMovesToEqualArrayElementsSolution
+    {
+        public int MinMoves(int[] nums) {
+            int min = Int32.MaxValue;
+            int len = nums.Length;
+            int sum = 0;
+
+            for (int i = 0; i < len; i++)
+            {
+                sum += nums[i];
+                min = Math.Min(min, nums[i]);
+            }
+            return sum - min * len;
+        }
+    }
+}
