9addc553c9b33b2e5ad20c0dedf3800736143b42
yli152 <41751701+yli152@users.noreply.github.com>
2018-08-28 16:07:51 -0400

    Add files via upload

---
+++ RangeAdditionIISolution.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    public class RangeAdditionIISolution
+    {
+        public int MaxCount(int m, int n, int[,] ops) {
+            int x = m;
+            int y = n;
+            int len = ops.Length;
+            for (int i = 0; i < len; i++)
+            {
+                if (ops[i,0] <= x)
+                {
+                    x = ops[i, 0];
+                }
+                if (ops[i,1] <= y)
+                {
+                    y = ops[i, 1];
+                }
+            }
+            return x * y;
+        }
+    }
+}
