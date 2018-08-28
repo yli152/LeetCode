99731b36c16ece45e8a2991032f4cf32217fc58f
yli152 <41751701+yli152@users.noreply.github.com>
2018-08-28 16:34:47 -0400

    Add files via upload

---
+++ SumOfSquareNumbersSolution.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    public class SumOfSquareNumbersSolution
+    {
+        public bool JudgeSquareSum(int c) {
+            int limit = (int)Math.Sqrt(c);
+
+            for (int i = 0; i <= limit; i++) {
+                int temp = i * i;
+                int res = (int)Math.Sqrt(c - temp);
+                if ((res * res + temp) == c) {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
