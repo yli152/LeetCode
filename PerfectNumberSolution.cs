06adfa262f4ee9a43b97dfbf510458c2613a885a
yli152 <41751701+yli152@users.noreply.github.com>
2018-08-28 15:42:43 -0400

    Add files via upload

---
+++ PerfectNumberSolution.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    public class PerfectNumberSolution
+    {
+        public bool CheckPerfectNumber(int num) {
+            //int result = 0;
+
+            //for (int i = 1; i < Math.Sqrt(num); i++) {
+            //    if (num % i == 0) {
+            //        result += i;
+            //    }
+            //}
+
+            //return result == num;
+            return num == 6 || num == 28 || num == 496 || num == 8128 || num == 33550336;
+        }
+    }
+}
