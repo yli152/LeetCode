5b9fa8dd2d1672675fb2eea6c867d1f185995578
yli152 <41751701+yli152@users.noreply.github.com>
2018-08-29 15:26:57 -0400

    Add files via upload

---
+++ LargestTriangleAreaSolution.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    //Heron's formula S = (A + B + C) / 2
+    public class LargestTriangleAreaSolution
+    {
+        public double LargestTriangleArea(int[][] points)
+        {
+            int N = points.Length;
+            double result = 0;
+
+            for (int i = 0; i < N; i++)
+                for (int j = i + 1; j < N; j++)
+                    for (int k = j + 1; k < N; k++)
+                        result = Math.Max(result, Area(points[i], points[j], points[k]));
+            return result;
+        }
+
+        public double Area(int[] P, int[] Q, int[] R)
+        {
+            return 0.5 * Math.Abs(P[0] * Q[1] + Q[0] * R[1] + R[0] * P[1] - P[1] * Q[0] - Q[1] * R[0] - R[1] * P[0]);
+        }
+    }
+}
