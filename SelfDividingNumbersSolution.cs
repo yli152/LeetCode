d3a12b8e48180dacf0fab676ab9f70aeb31018b3
yli152 <41751701+yli152@users.noreply.github.com>
2018-08-29 14:22:56 -0400

    Add files via upload

---
+++ SelfDividingNumbersSolution.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    public class SelfDividingNumbersSolution
+    {
+        public IList<int> SelfDividingNumbers(int left, int right) {
+            IList<int> result = new List<int>();
+
+            for (int i = left; i <= right; i++) {
+                bool flag = true;
+                int temp = i;
+                while (temp != 0) {
+                    int digit = temp % 10;
+                    if (digit == 0 || i % digit != 0) {
+                        flag = false;
+                        break;
+                    }
+                    temp /= 10;
+                }
+                if (flag) {
+                    result.Add(i);
+                }
+            }
+
+            return result;
+        }
+    }
+}
