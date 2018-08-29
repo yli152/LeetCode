f5ea439a037cacf0dafeaa76ef8d4a4ec6a25d74
yli152 <41751701+yli152@users.noreply.github.com>
2018-08-29 15:48:23 -0400

    Add files via upload

---
+++ RectangleOverlapSolution.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    public class RectangleOverlapSolution
+    {
+        public bool IsRectangleOverlap(int[] rec1, int[] rec2) {
+            return !(rec1[2] <= rec2[0] ||   // left
+                 rec1[3] <= rec2[1] ||   // bottom
+                 rec1[0] >= rec2[2] ||   // right
+                 rec1[1] >= rec2[3]);    // top
+        }
+    }
+}
