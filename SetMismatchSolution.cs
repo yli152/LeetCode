8771b9a0033af779cc6ddbde56111dc75b138466
yli152 <41751701+yli152@users.noreply.github.com>
2018-08-29 11:10:57 -0400

    Add files via upload

---
+++ SetMismatchSolution.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    public class SetMismatchSolution
+    {
+        public int[] FindErrorNums(int[] nums) {
+            int len = nums.Length;
+            int[] hash = new int[len + 1];
+            int duplicate = 0;
+            int loss = 0;
+
+            for (int i = 0; i < len; i++)
+            {
+                hash[nums[i]]++;
+            }
+
+            for (int i = 1; i < len + 1; i++)
+            {
+                if (hash[i] == 2) duplicate = i;
+                if (hash[i] == 0) loss = i;
+            }
+
+            int[] res = new int[2];
+            res[0] = duplicate;
+            res[1] = loss;
+            return res;
+        }
+    }
+}
