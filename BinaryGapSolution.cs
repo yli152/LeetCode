73e7aec4c69b077434d54e6ccc9261378adbce93
yli152 <41751701+yli152@users.noreply.github.com>
2018-08-29 16:09:16 -0400

    Add files via upload

---
+++ BinaryGapSolution.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    public class BinaryGapSolution
+    {
+        public int BinaryGap(int N) {
+            int res = 0;
+            for (int d = -32; N > 0; N /= 2, d++)
+                if (N % 2 == 1)
+                {
+                    res = Math.Max(res, d);
+                    d = 0;
+                }
+            return res;
+        }
+    }
+}
