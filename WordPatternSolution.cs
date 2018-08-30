dba022dd95e29fb01408f74af7773b62a1250dff
yli152 <41751701+yli152@users.noreply.github.com>
2018-08-30 11:00:56 -0400

    Add files via upload

---
+++ WordPatternSolution.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    public class WordPatternSolution
+    {
+        // Same as Isismorphic string solution
+        public bool WordPattern(string pattern, string str) {
+            string[] arr = str.Split(' ');
+            int len = pattern.Length;
+            if (len != arr.Length) {
+                return false;
+            }
+            Dictionary<char, string> dc = new Dictionary<char, string>();
+
+            for (int i = 0; i < len; i++) {
+                if (dc.Keys.Contains(pattern[i]))
+                {
+                    if (arr[i] != dc[pattern[i]]) {
+                        return false;
+                    } 
+                }
+                else {
+                    if (dc.Values.Contains(arr[i])) {
+                        return false;
+                    }
+                    dc[pattern[i]] = arr[i];
+                }
+            }
+
+            return true;
+        }
+    }
+}
