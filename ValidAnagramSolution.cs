2bc1885f036dbf6dc5dda718021d3b6052652782
yli152 <41751701+yli152@users.noreply.github.com>
2018-08-30 16:33:34 -0400

    Add files via upload

---
+++ ValidAnagramSolution.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    public class ValidAnagramSolution
+    {
+        //sort
+        public bool IsAnagram(string s, string t) {
+            char[] sarr = s.ToCharArray();
+            Array.Sort(sarr);
+            char[] tarr = t.ToCharArray();
+            Array.Sort(tarr);
+            int lens = sarr.Length;
+            int lent = tarr.Length;
+            if (lens != lent) {
+                return false;
+            }
+
+            for (int i = 0; i < lens; i++) {
+                if (sarr[i] != tarr[i]) {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
