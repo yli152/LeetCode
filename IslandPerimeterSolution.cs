bb2ad8cabcce655bd416027989ead15d33d895dc
yli152 <41751701+yli152@users.noreply.github.com>
2018-09-04 16:26:49 -0400

    Add files via upload

---
+++ IslandPerimeterSolution.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    //O(N)2 check四边
+    public class IslandPerimeterSolution
+    {
+        public int IslandPerimeter(int[,] grid) {
+            int row = grid.GetLength(0);
+            int col = grid.GetLength(1);
+            int result = 0;
+
+            for (int i = 0; i < row; i++) {
+                for (int j = 0; j < col; j++) {
+                    if (grid[i, j] == 1) {
+                        if (i == 0 || grid[i - 1, j] == 0) {
+                            result++;
+                        }
+                        if (j == 0 || grid[i, j - 1] == 0) {
+                            result++;
+                        }
+                        if (i == row - 1 || grid[i + 1, j] == 0) {
+                            result++;
+                        }
+                        if (j == col - 1 || grid[i, j + 1] == 0) {
+                            result++;
+                        }
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
