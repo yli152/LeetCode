ba2d80bb96e5c4d750ae23599e8dc2765771b566
yli152 <41751701+yli152@users.noreply.github.com>
2018-09-04 10:30:52 -0400

    Add files via upload

---
+++ FindTheDifferenceSolution.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    public class FindTheDifferenceSolution
+    {
+        //Dictionary dc[t[i]]--
+        public char FindTheDifference(string s, string t) {
+            int lens = s.Length;
+            int lent = t.Length;
+            Dictionary<char, int> dc = new Dictionary<char, int>();
+
+            for (int i = 0; i < lens; i++) {
+                if (dc.Keys.Contains(s[i]))
+                {
+                    dc[s[i]]++;
+                }
+                else {
+                    dc[s[i]] = 1;
+                }
+            }
+
+            for (int i = 0; i < lent; i++) {
+                if (dc.Keys.Contains(t[i])) {
+                    dc[t[i]]--;
+                }
+            }
+
+            foreach (var pair in dc) {
+                if (pair.Value != 0) {
+                    return pair.Key;
+                }
+            }
+
+            return t[lent - 1];
+        }
+    }
+}
