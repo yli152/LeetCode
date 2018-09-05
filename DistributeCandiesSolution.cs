ca8118791cfbddeca4ef45dad6748def21438a2b
yli152 <41751701+yli152@users.noreply.github.com>
2018-09-05 11:32:32 -0400

    Add files via upload

---
+++ DistributeCandiesSolution.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Leetcode
+{
+    public class DistributeCandiesSolution
+    {
+        public int DistributeCandies(int[] candies) {
+            int len = candies.Length;
+            HashSet<int> hs = new HashSet<int>();
+
+            for (int i = 0; i < len; i++) {
+                hs.Add(candies[i]);
+            }
+
+            return hs.Count >= candies.Length / 2 ? candies.Length / 2 : hs.Count;
+        }
+    }
+}
