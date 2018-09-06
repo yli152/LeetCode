49486eaf75660b0e7789f49c16444f91059f5ef8
yli152 <41751701+yli152@users.noreply.github.com>
2018-09-06 16:04:12 -0400

    Add files via upload

---
+++ UncommonWordsFromTwoSentencesSolution.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    //Two Dictionaries
+    public class UncommonWordsFromTwoSentencesSolution
+    {
+        public string[] UncommonFromSentences(string A, string B) {
+            Dictionary<string, int> dca = new Dictionary<string, int>();
+            Dictionary<string, int> dcb = new Dictionary<string, int>();
+            string[] arra = A.Split(' ');
+            string[] arrb = B.Split(' ');
+            int lena = arra.Length;
+            int lenb = arrb.Length;
+            int idx = 0;
+
+            while (idx < lena || idx < lenb) {
+                if (idx < lena) {
+                    if (dca.Keys.Contains(arra[idx]))
+                    {
+                        dca[arra[idx]]++;
+                    }
+                    else {
+                        dca[arra[idx]] = 1;
+                    }
+                }
+
+                if (idx < lenb)
+                {
+                    if (dcb.Keys.Contains(arrb[idx]))
+                    {
+                        dcb[arrb[idx]]++;
+                    }
+                    else
+                    {
+                        dcb[arrb[idx]] = 1;
+                    }
+                }
+                idx++;
+            }
+
+            List<string> ls = new List<string>();
+            foreach (var pair in dca) {
+                if (pair.Value == 1 && !dcb.Keys.Contains(pair.Key)) {
+                    ls.Add(pair.Key);
+                }
+            }
+            foreach (var pair in dcb)
+            {
+                if (pair.Value == 1 && !dca.Keys.Contains(pair.Key))
+                {
+                    ls.Add(pair.Key);
+                }
+            }
+
+            string[] result = new string[ls.Count];
+            int i = 0;
+            foreach (var s in ls) {
+                result[i++] = s;
+            }
+
+            return result;
+        }
+    }
+}
