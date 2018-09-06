90d1d10729ce9ae1aab69c745d50b0f203489771
yli152 <41751701+yli152@users.noreply.github.com>
2018-09-06 15:29:29 -0400

    Add files via upload

---
+++ SubdomainVisitCountSolution.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    public class SubdomainVisitCountSolution
+    {
+        public IList<string> SubdomainVisits(string[] cpdomains) {
+            Dictionary<string, int> dic = new Dictionary<string, int>();
+
+            foreach (string sub in cpdomains)
+            {
+                string[] ss = sub.Split(' ');
+                int count = int.Parse(ss[0]);
+
+                string[] sss = ss[1].Split('.');
+                string temp = "";
+                for (int i = sss.Length - 1; i >= 0; i--)
+                {
+                    temp = string.IsNullOrEmpty(temp) ?
+                        sss[i] : string.Format("{0}{1}{2}", sss[i], '.', temp);
+
+                    if (!dic.ContainsKey(temp))
+                    {
+                        dic.Add(temp, count);
+                    }
+                    else
+                    {
+                        dic[temp] += count;
+                    }
+                }
+            }
+            return dic.Select(it => string.Format("{0} {1}", it.Value, it.Key)).ToList();
+        }
+    }
+}
