3137d6859169aed536f3bd8389174ee0303f1f6c
yli152 <41751701+yli152@users.noreply.github.com>
2018-09-06 13:35:21 -0400

    Add files via upload

---
+++ LongestWordInDictionarySolution.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    //HashSet 1. Sort 2. hs.Contains(0, word.Length -1)
+    public class LongestWordInDictionarySolution
+    {
+        public string LongestWord(string[] words) {
+            Array.Sort(words);
+            HashSet<string> hs = new HashSet<string>();
+            string result = "";
+
+            foreach (var word in words) {
+                int len = word.Length;
+                if (len == 1 || hs.Contains(word.Substring(0, len - 1))) {
+                    hs.Add(word);
+                    if (len > result.Length) {
+                        result = word;
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
