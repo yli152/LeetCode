609af8aee409bba5c2e1e2415f0d27d3a5a82295
yli152 <41751701+yli152@users.noreply.github.com>
2018-09-07 09:56:57 -0400

    Add files via upload

---
+++ DesignHashSetSolution.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    public class DesignHashSetSolution
+    {
+        bool[] arr;
+        /** Initialize your data structure here. */
+        public DesignHashSetSolution()
+        {
+            arr = new bool[1000000];
+        }
+
+        public void Add(int key)
+        {
+            arr[key] = true;
+        }
+
+        public void Remove(int key)
+        {
+            arr[key] = false;
+        }
+
+        /** Returns true if this set contains the specified element */
+        public bool Contains(int key)
+        {
+            return arr[key];
+        }
+    }
+
+    /**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet obj = new MyHashSet();
+ * obj.Add(key);
+ * obj.Remove(key);
+ * bool param_3 = obj.Contains(key);
+ */
+}
