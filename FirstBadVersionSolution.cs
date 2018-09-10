a238089e3591ef06f0278d8b98b9a20a8d5eddc4
yli152 <41751701+yli152@users.noreply.github.com>
2018-09-10 09:59:54 -0400

    Add files via upload

---
+++ FirstBadVersionSolution.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    public class FirstBadVersionSolution
+    {
+        public int FirstBadVersion(int n) {
+            int left = 1;
+            int right = n;
+
+            while (left < right) {
+                int mid = left + (right - left) / 2;
+                if (isBadVersion(mid)) {
+                    right = mid;
+                } else {
+                    left = mid + 1;
+                }
+            }
+
+            return left;
+        }
+    }
+}
