5574b24fe35d83bc7e5525084d1f6f3d390273b7
yli152 <41751701+yli152@users.noreply.github.com>
2018-09-12 16:52:36 -0400

    Add files via upload

---
+++ PeakIndexInAMountainArraySolution.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    public class PeakIndexInAMountainArraySolution
+    {
+        public int PeakIndexInMountainArray(int[] A) {
+            int len = A.Length;
+            int result = 0;
+
+            for (int i = 1; i < len - 1; i++)
+            {
+                if (A[i] >= A[i - 1] && A[i] >= A[i + 1])
+                {
+                    result = i;
+                }
+            }
+
+            return result;
+        }
+    }
+}
