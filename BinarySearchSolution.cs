f4efa35f10309287c442e669f25e3470cab946ad
yli152 <41751701+yli152@users.noreply.github.com>
2018-09-13 11:10:40 -0400

    Add files via upload

---
+++ BinarySearchSolution.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    public class BinarySearchSolution
+    {
+        public int Search(int[] nums, int target) {
+            int len = nums.Length;
+            int left = 0;
+            int right = len - 1;
+
+            while (left <= right) {
+                int mid = left + (right - left) / 2;
+                if (nums[mid] == target) {
+                    return mid;
+                } else if (nums[mid] < target) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
