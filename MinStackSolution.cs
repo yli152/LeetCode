1f2f4c08a08c0aae86d773aa69768d2869b78d35
yli152 <41751701+yli152@users.noreply.github.com>
2018-09-20 10:00:37 -0400

    Add files via upload

---
+++ MinStackSolution.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    //Two stacks Min stack is to store current min value
+    public class MinStackSolution
+    {
+        Stack<int> Values;
+        Stack<int> Min;
+        /** initialize your data structure here. */
+        public MinStackSolution()
+        {
+            Values = new Stack<int>();
+            Min = new Stack<int>();
+        }
+
+        public void Push(int x)
+        {
+            Values.Push(x);
+            if (Min.Count == 0 || x <= GetMin())
+            {
+                Min.Push(x);
+            }
+        }
+
+        public void Pop()
+        {
+            int popped = Values.Pop();
+            if (popped == GetMin())
+            {
+                Min.Pop();
+            }
+        }
+
+        public int Top()
+        {
+            return Values.Count == 0 ? 0 : Values.Peek();
+        }
+
+        public int GetMin()
+        {
+            return Min.Count == 0 ? 0 : Min.Peek();
+        }
+    }
+
+    /**
+     * Your MinStack object will be instantiated and called as such:
+     * MinStack obj = new MinStack();
+     * obj.Push(x);
+     * obj.Pop();
+     * int param_3 = obj.Top();
+     * int param_4 = obj.GetMin();
+     */
+}
