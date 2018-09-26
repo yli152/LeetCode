0107299e33039ce72b26d8c96c5e61b33e47dbd0
yli152 <41751701+yli152@users.noreply.github.com>
2018-09-26 15:57:50 -0400

    Add files via upload

---
+++ MiddleOfTheLinkedListSolution.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    //Fast & slow pointers
+    public class MiddleOfTheLinkedListSolution
+    {
+        public ListNode MiddleNode(ListNode head) {
+            ListNode slow = head, fast = head;
+            while (fast != null && fast.next != null)
+            {
+                slow = slow.next;
+                fast = fast.next.next;
+            }
+            return slow;
+        }
+    }
+}
