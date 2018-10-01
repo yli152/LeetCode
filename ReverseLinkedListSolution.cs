580efc87455d1f0004052642d31586414d5b9fcc
yli152 <41751701+yli152@users.noreply.github.com>
2018-10-01 11:07:00 -0400

    Add files via upload

---
+++ ReverseLinkedListSolution.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    public class ReverseLinkedListSolution
+    {
+        public ListNode ReverseList(ListNode head) {
+            if (head == null)
+            {
+                return null;
+            }
+
+            ListNode first = head;
+            ListNode cur = head;
+            while (cur.next != null)
+            {
+                var next = cur.next;
+                cur.next = next.next;
+                next.next = first;
+                first = next;
+            }
+
+            return first;
+        }
+    }
+}
