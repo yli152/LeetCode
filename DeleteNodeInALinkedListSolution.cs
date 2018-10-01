be89861af469fa95cdce690ff7d402ff34babab8
yli152 <41751701+yli152@users.noreply.github.com>
2018-10-01 11:34:49 -0400

    Add files via upload

---
+++ DeleteNodeInALinkedListSolution.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    //Copy the next node to the given node and delete the next one
+    public class DeleteNodeInALinkedListSolution
+    {
+        public void DeleteNode(ListNode node) {
+            node.val = node.next.val;
+            node.next = node.next.next;
+        }
+    }
+}
