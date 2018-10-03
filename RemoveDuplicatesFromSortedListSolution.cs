ac7e0f0219fa001794774f396ed0522ba1fa2437
yli152 <41751701+yli152@users.noreply.github.com>
2018-10-03 10:16:47 -0400

    Add files via upload

---
+++ RemoveDuplicatesFromSortedListSolution.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    public class RemoveDuplicatesFromSortedListSolution
+    {
+        public ListNode DeleteDuplicates(ListNode head) {
+            if (head == null) {
+                return null;
+            }
+            ListNode node = head;
+
+            while (node != null && node.next != null) {
+                if (node.val == node.next.val)
+                {
+                    node.next = node.next.next;
+                }
+                else {
+                    node = node.next;
+                }
+            }
+
+            return head;
+        }
+    }
+}
