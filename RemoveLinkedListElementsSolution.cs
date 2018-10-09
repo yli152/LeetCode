46d6d58f3d6c6f0fcccc832e6c63fc2635a74538
yli152 <41751701+yli152@users.noreply.github.com>
2018-10-09 14:32:41 -0400

    Add files via upload

---
+++ RemoveLinkedListElementsSolution.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    public class RemoveLinkedListElementsSolution
+    {
+        //DummyHead
+        public ListNode RemoveElements(ListNode head, int val) {
+            if (head == null)
+            {
+                return null;
+            }
+
+            ListNode dummyHead = new ListNode(-1);
+            dummyHead.next = head;
+
+            ListNode curNode = dummyHead;
+
+            while (curNode.next != null) {
+                if (curNode.next.val == val)
+                {
+                    curNode.next = curNode.next.next;
+                }
+                else {
+                    curNode = curNode.next;
+                }
+            }
+
+            return dummyHead.next;
+        }
+    }
+}
