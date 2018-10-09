b1bed7e599a1c46f5e3f8554aa2e7ee2c07349e4
yli152 <41751701+yli152@users.noreply.github.com>
2018-10-09 10:38:10 -0400

    Add files via upload

---
+++ LinkedListCycleSolution.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    //Slow and Fast two pointers 
+    public class LinkedListCycleSolution
+    {
+        public bool HasCycle(ListNode head) {
+            if (head == null) {
+                return false;
+            }
+            ListNode slow = head;
+            ListNode fast = head;
+
+            while (fast.next != null && fast.next.next != null) {
+                slow = slow.next;
+                fast = fast.next.next;
+                if (slow == fast) {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
